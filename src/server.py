b759a2c2a3c2f8e2b9009be38e268dd0ccc2f1d8
Omar Kidwai <omar.kidwai@hotmail.com>
2022-02-11 11:08:06 +0000

    Moved server

--- src/server.py
+++ src/server.py
@@ -6,33 +6,44 @@ from generated.server import train_api_pb2 as protos
 
 class TrainAPIServicer(api.TrainAPIServicer):
     def __init__(self):
+        # Initial Train
         initTrain = protos.Train()
         initTrain.name = "Python Train"
         initTrain.id = 0
         initTrain.operator = protos.Operator.OPERATOR_THAMESLINK
         initTrain.train_type = protos.TrainType.TRAIN_TYPE_HIGH_SPEED
         initTrain.coach_count = 5
-        
+        # Counter to use for train IDs
         self.trainCount = 1
+        # List of trains
         self.trains = [initTrain]
     
     def ListTrains(self, request, context):
         response = protos.ListTrainsResponse()
+
+        # TODO: Set the list of trains to be returned
         response.trains.extend(self.trains)
+
         return response
     
     def CreateTrain(self, request, context):
         train = request.train
         train.id = self.trainCount
         self.trainCount += 1
+        
+        # TODO: Add the new train to trains
         self.trains.append(train)
+
         response = protos.CreateTrainResponse()
         return response
     
     def DeleteTrain(self, request, context):
         trainId = request.id
+
+        # TODO: Remove the train with id=trainId from trains
         self.trains = [train for train in self.trains if train.id != trainId]
         #self.trains = filter(lambda train: train.getId() != trainId, self.trains)
+        
         response = protos.DeleteTrainResponse()
         return response
     
